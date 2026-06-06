529e254ba791e2e29f1ce1b1875eb3f8f5ce974f
Kamil Klimas <kamil.klimas1@gmail.com>
2026-06-06 19:42:01 +0200

    US-2: Add Patient DTOs and Mapperly mapper

---
+++ Clinic_Manager/Clinic_Manager/DTOs/CreatePatientDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Clinic_Manager.DTOs;
+
+public class CreatePatientDto
+{
+    [Required(ErrorMessage = "PESEL jest wymagany.")]
+    [RegularExpression(@"^\d{11}$", ErrorMessage = "PESEL musi składać się z dokładnie 11 cyfr.")]
+    public string Pesel { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Imię jest wymagane.")]
+    [StringLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Nazwisko jest wymagane.")]
+    [StringLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Data urodzenia jest wymagana.")]
+    [DataType(DataType.Date)]
+    public DateOnly DateOfBirth { get; set; }
+
+    [Phone(ErrorMessage = "Nieprawidłowy numer telefonu.")]
+    [StringLength(20)]
+    public string? PhoneNumber { get; set; }
+
+    [EmailAddress(ErrorMessage = "Nieprawidłowy adres e-mail.")]
+    [StringLength(256)]
+    public string? Email { get; set; }
+
+    [StringLength(250)]
+    public string? Address { get; set; }
+}
